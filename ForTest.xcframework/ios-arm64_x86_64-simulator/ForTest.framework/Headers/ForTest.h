5c4ffe632be3ada7f9cb0a848d7dbfa7bb863164
Long Vũ <66582805+caolong0204@users.noreply.github.com>
2023-04-02 12:08:08 +0700

    develop

---
+++ ForTest.xcframework/ios-arm64_x86_64-simulator/ForTest.framework/Headers/ForTest.h
@@ -0,0 +1,18 @@
+//
+//  ForTest.h
+//  ForTest
+//
+//  Created by Thanh Long on 02/04/2023.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ForTest.
+FOUNDATION_EXPORT double ForTestVersionNumber;
+
+//! Project version string for ForTest.
+FOUNDATION_EXPORT const unsigned char ForTestVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ForTest/PublicHeader.h>
+
+
